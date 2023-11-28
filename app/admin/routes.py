c1ce667fe11ecf3bc8627a8dc1a9bc9f47f36b58
anhilmy <hilmyahmadnaufal@gmail.com>
2023-11-28 15:52:28 +0700

    feat: backend commit

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,6 +1,25 @@
+from flask import render_template, request, url_for, redirect
 from app.admin import bp
+from app.extensions import db
+from app.models.display_object import DisplayObject,DisplayObjectType
 
 
-@bp.route("/")
+@bp.route("/", methods=("get"))
 def index():
-    return "admin blueprint"
+    display_objects = DisplayObject.query.all()
+    return render_template('admin/index.html', list=display_objects)
+
+@bp.route("/new", methods=("get", "post"))
+def new():
+    if request.method == "POST":
+        result = DisplayObject(
+            link= request.form["link"],
+            display_name = request.form["display_name"],
+            is_active = True,
+            display_type = DisplayObjectType.LINK
+        )
+        db.session.add(result)
+        db.session.commit()
+        
+        return render_template("admin/new.html", result=result)
+    return render_template('admin/new.html')
