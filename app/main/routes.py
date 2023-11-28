3e4532c79600c2b4ec288cf3db7c3cf77a10278d
anhilmy <hilmyahmadnaufal@gmail.com>
2023-11-28 14:27:47 +0700

    feat: add styling

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,10 +1,12 @@
 from flask import render_template
 from app.main import bp
+from app.models.display_object import DisplayObject
 
 
 @bp.route("/")
 def index():
-    return render_template('main/index.html')
+    display_objects = DisplayObject.query.all()
+    return render_template('main/index.html', list=display_objects)
 
 @bp.route("/login")
 def login():
